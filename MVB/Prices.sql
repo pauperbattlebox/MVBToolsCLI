27f8602850eb457ac60b5cdaaa228393e3d0c2d7
JMoo <pauperbattlebox@gmail.com>
2023-02-08 16:44:40 -0600

    tables rebuild

--- MVB/Prices.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Prices]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [CsId] INT NOT NULL, 
-    [CsPrice] FLOAT NULL, 
-    [ScryfallPrice] FLOAT NULL, 
-    [CardKingdomPrice] FLOAT NULL
-)
---
+++ MVB/dbo/Tables/Prices.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Prices]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CsId] INT NOT NULL, 
+    [CsPrice] FLOAT NULL, 
+    [ScryfallPrice] FLOAT NULL, 
+    [CardKingdomPrice] FLOAT NULL
+)
