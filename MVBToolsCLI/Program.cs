5a16b133d943289358eca5a07f84fbbcea51d1fd
jmoo <pauperbattlebox@gmail.com>
2023-03-17 23:03:42 -0500

    remove commentary

--- MVBToolsCLI/Program.cs
+++ MVBToolsCLI/Program.cs
@@ -39,7 +39,6 @@ namespace MVBToolsCLI
                 name: "editionId",
                 description: "Edition ID.");
 
-
             var addEditionCommand = new Command("addEdition", "Add edition to db by Cardsphere ID.")            
             {
                 editionIdArgument
@@ -105,7 +104,6 @@ namespace MVBToolsCLI
                 scryfallCardIdOption
             };
 
-
             addPriceCommand.SetHandler((source, csId, scryfallId) =>
             {
                 if (source == "cardsphere")
@@ -118,110 +116,12 @@ namespace MVBToolsCLI
                 }
             }, priceSourceOption, csCardIdOption, scryfallCardIdOption);
 
-
-            rootCommand.AddCommand(addPriceCommand);
-            
+            rootCommand.AddCommand(addPriceCommand);            
 
 
             return await rootCommand.InvokeAsync(args);
 
-
-            //bool continueProgram = true;
-
-            //while (continueProgram == true)
-            //{
-            //    Console.Write("Enter a command (viewsets, addset, viewcard, viewcardprice, addcardsbyset, addallcards, addprice, exit): ");
-
-            //    string option = Console.ReadLine();
-
-            //    if (option == "addset")
-            //    {
-            //        Console.WriteLine("What set ID would you like to add: ");
-
-            //        string setId = Console.ReadLine();
-
-            //        Commands.AddNewEditionToDb(Int32.Parse(setId), sqlConnection, consoleWriter);
-            //    }
-
-            //    if (option == "viewsets")
-            //    {
-            //        Commands.GetEditionsFromDb(sqlConnection, consoleWriter);
-            //    }
-
-            //    if (option == "viewcard")
-            //    {
-            //        Console.WriteLine("What card ID would you like to view: ");
-
-            //        string csId = Console.ReadLine();
-
-            //        Commands.GetCardFromDb(sqlConnection, Int32.Parse(csId), consoleWriter);
-            //    }
-
-            //    if (option == "viewcardprice")
-            //    {
-            //        Console.WriteLine("What card ID would you like to view a price for: ");
-
-            //        string csId = Console.ReadLine();
-
-            //        Commands.GetCardPriceFromDb(sqlConnection, Int32.Parse(csId), consoleWriter);
-            //    }
-
-            //    if (option == "addcardsbyset")
-            //    {
-            //        Console.WriteLine("What set ID would you likd to add cards for: ");
-
-            //        string setId = Console.ReadLine();
-
-            //        Commands.AddCardsToDbByEdition(Int32.Parse(setId), sqlConnection, consoleWriter);
-            //    }
-
-            //    if (option == "addallcards")
-            //    {
-            //        Console.WriteLine("This will attempt to process close to 100k cards, proceed(y/n)");
-
-            //        string areYouSure = Console.ReadLine();
-
-            //        if (areYouSure == "y")
-            //        {
-            //            Commands.AddCardsToDbFromJsonFile(sqlConnection, "all_ids.json", consoleWriter, fileReader);
-            //        };
-            //    }
-
-            //    if (option == "addprice")
-            //    {
-            //        Console.WriteLine("Would you like to add from MVB or Scryfall (mvb, scryfall): ");
-
-            //        string api = Console.ReadLine();
-
-            //        if (api == "mvb")
-            //        {
-            //            Console.WriteLine("Enter the Cardsphere ID: ");
-
-            //            string csId = Console.ReadLine();
-
-            //            Commands.RefreshMVBPriceInDb(Int32.Parse(csId), sqlConnection, consoleWriter);
-            //        }
-
-            //        if (api == "scryfall")
-            //        {
-            //            Console.WriteLine("Enter the Scryfall ID: ");
-
-            //            string scryfallId = Console.ReadLine();
-
-            //            Commands.RefreshScryfallPriceInDb(scryfallId, sqlConnection, consoleWriter);
-            //        }
-            //    }
-
-            //    if (option == "exit")
-            //    {
-            //        continueProgram = false;
-            //    }
-                                    
-            //}
-
-            Console.WriteLine("That's the end");
-
-            Console.ReadLine();
+            //    Console.Write("Enter a command (viewcardprice, addallcards): ");
 
             static string GetConnectionString(string connectionStringName = "Default")
             {
