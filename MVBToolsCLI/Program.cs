9b4615558bc24ba5d110d84301cdbf98f25ff333
jmoo <pauperbattlebox@gmail.com>
2023-03-17 22:54:09 -0500

    CL options and arguments

--- MVBToolsCLI/Program.cs
+++ MVBToolsCLI/Program.cs
@@ -6,6 +6,7 @@ using MVBToolsLibrary.Json;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.ComponentModel;
+using System.Diagnostics.Tracing;
 
 namespace MVBToolsCLI
 {
@@ -21,6 +22,7 @@ namespace MVBToolsCLI
 
             var rootCommand = new RootCommand();            
 
+            //Get editions from db
             var getEditionsCommand = new Command("edition", "Get all editions from db.");
 
             getEditionsCommand.SetHandler(boolparam =>
@@ -31,32 +33,93 @@ namespace MVBToolsCLI
 
             rootCommand.AddCommand(getEditionsCommand);
 
-            var editionIdOption = new Option<int>(
-                name: "--editionId",
-                description: "Edition ID to add to db.");
+
+            //Add edition to db
+            var editionIdArgument = new Argument<int>(
+                name: "editionId",
+                description: "Edition ID.");
 
 
-            var addEditionCommand = new Command("addEdition", "Add edition to db by Cardsphere ID.")
+            var addEditionCommand = new Command("addEdition", "Add edition to db by Cardsphere ID.")            
             {
-                editionIdOption
+                editionIdArgument
             };
 
             addEditionCommand.SetHandler((editionId) =>
             {
                 Commands.AddNewEditionToDb(editionId, sqlConnection, consoleWriter);
-            }, editionIdOption);
+            }, editionIdArgument);
 
             rootCommand.AddCommand(addEditionCommand);
 
-            
+            //View card
+            var csCardIdArgument = new Argument<int>(
+                name: "cardId",
+                description: "Card ID.");
+
+            var getCardCommand = new Command("getCard", "Get card from db by Cardshpere ID.")
+            {
+                csCardIdArgument
+            };
+
+            getCardCommand.SetHandler((cardId) =>
+            {
+                Commands.GetCardFromDb(sqlConnection, cardId, consoleWriter);
+            }, csCardIdArgument);
+
+            rootCommand.AddCommand(getCardCommand);
+
+            //Add cards by set
+            var addCardsByEdition = new Command("addCardsByEdition", "Add all cards from a given edition to db.")
+            {
+                editionIdArgument
+            };
+
+            addCardsByEdition.SetHandler((editionId) =>
+            {
+                Commands.AddCardsToDbByEdition(editionId, sqlConnection, consoleWriter);
+            }, editionIdArgument);
+
+            rootCommand.AddCommand(addCardsByEdition);
+
+            //Add price
+            var scryfallCardIdOption = new Option<string>(
+                name: "--scryfallCardId",
+                description: "Scryfall card ID");
+            scryfallCardIdOption.AddAlias("-s");
+
+            var csCardIdOption = new Option<int>(
+                name: "--cardsphereId",
+                description: "Cardsphere ID");
+            csCardIdOption.AddAlias("-c");
+
+            var priceSourceOption = new Option<string>(
+                name: "--priceSource",
+                description: "Source to add price from").FromAmong("cardsphere", "scryfall");
+            priceSourceOption.AddAlias("-p");
+
+            var addPriceCommand = new Command("addPrice", "Add or update card price")
+            {
+                priceSourceOption,
+                csCardIdOption,
+                scryfallCardIdOption
+            };
 
 
+            addPriceCommand.SetHandler((source, csId, scryfallId) =>
+            {
+                if (source == "cardsphere")
+                {
+                    Commands.RefreshMVBPriceInDb(csId, sqlConnection, consoleWriter);
+                }
+                if (source == "scryfall")
+                {
+                    Commands.RefreshScryfallPriceInDb(scryfallId, sqlConnection, consoleWriter);
+                }
+            }, priceSourceOption, csCardIdOption, scryfallCardIdOption);
 
-            
 
-
-            
-
+            rootCommand.AddCommand(addPriceCommand);
             
 
 
